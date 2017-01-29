9ceba46d34e8d876c1e3675fa62eaacf2ec47c0c
jayyin <jayyin11043@hotmail.com>
2017-01-29 14:11:00 -0500

    BTS630 A1 main project
    contains full solution project from visual studio

---
+++ A1 -website UI/A1 - Website UI/default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace A1___Website_UI
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string returnUrl = "~/search.aspx";
+
+            // check if it exists, if not then redirect to default page
+            if (!(returnUrl == null))
+            Response.Redirect(returnUrl);
+        }
+    }
+}
---
+++ A1 -website UI/A1 - Website UI/search.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TrueMaster.Master" AutoEventWireup="true" CodeBehind="search.aspx.cs" Inherits="A1___Website_UI.search" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+
+<asp:Content ID="Content2" ContentPlaceHolderID="body1" runat="server">
+ 
+    
+        <!--<input id="Submit1" type="submit" value="submit" />-->
+</asp:Content>
